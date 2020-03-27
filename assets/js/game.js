c156f9446841695717599bec8056783756cc4de8
Dan Lyons <danielclyons@yahoo.com>
2020-03-26 17:58:54 -0700

    shop function, Sec 3.3.7

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,6 +24,16 @@ var startGame = function() {
       enemyHealth = 50;
   
       fight(pickedEnemyName);
+      // if we're not at the last enemy in the array
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+      // ask if user wants to use the store before next round
+      var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+      // if yes, take them to the store() function
+        if (storeConfirm){        
+      shop();
+      }
+      }
     } else {
       window.alert("You have lost your robot in battle! Game Over!");
       break;
@@ -58,6 +68,51 @@ else {
 }
 }
 
+var shop = function() {
+   // ask player what they'd like to do
+   var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+  );
+  window.alert("shopping");
+  // use switch to carry out action
+switch (shopOptionPrompt) {
+  case "REFILL": // new case
+  case "refill":
+    if (playerMoney >= 7) {
+      window.alert("Refilling player's health by 20 for 7 dollars.");
+
+      playerHealth = playerHealth + 20;
+      playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+
+    break;
+  case "UPGRADE": // new case
+  case "upgrade":
+    if (playerMoney >= 7) {
+      window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+      playerAttack = playerAttack + 6;
+      playerMoney = playerMoney - 7;
+    }
+    else {
+      window.alert("You don't have enough money!");
+    }
+
+    break;
+  case "LEAVE": // new case
+  case "leave":
+    window.alert("Leaving the store.");
+    break;
+  default:
+    window.alert("You did not pick a valid option. Try again.");
+    shop();
+    break;
+}
+}
+
 var fight = function(enemyNames) {
     // repeat and execute as long as the enemy robot is alive
     while(enemyHealth > 0 && playerHealth > 0){
@@ -79,11 +134,7 @@ var fight = function(enemyNames) {
           }  
             // if no (false), ask question again by running fight () again
   
-        }
-
-
-     
-           
+        }          
        
         //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
         enemyHealth = enemyHealth - playerAttack;
